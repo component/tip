fce0b2b754b50578900cdf70977829c6723efbb9
TJ Holowaychuk <tj@vision-media.ca>
2012-08-02 16:13:27 -0700

    add support for passing new markup

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function tip(el) {
  * @api public
  */
 
-function Tip(content) {
+function Tip(content, html) {
   if (!(this instanceof Tip)) return tip(content);
   Emitter.call(this);
   this.pad = 5;
   this.preferred = 'north';
   this.content = content;
-  this.el = $(render('tip'));
+  this.el = $(html || render('tip'));
   this.inner = this.el.find('.tip-inner');
   this.position('auto');
   if (Tip.effect) this.effect(Tip.effect);
